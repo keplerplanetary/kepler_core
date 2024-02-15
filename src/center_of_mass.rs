19f5f263a02f12a4406a56f85cdb43a1a77280b7
Christoph Beberweil <christoph.beberweil@protonmail.com>
2024-02-15 21:12:03 +0100

    fix: Center of mass divide by total mass
    Co-authored-by: Angel Hudgins <angel@angelh.dev>

--- src/center_of_mass.rs
+++ src/center_of_mass.rs
@@ -4,10 +4,14 @@ use crate::types::System;
 
 /// Calculates the center of mass of a system
 pub fn calculcate_center_of_mass(system: &System) -> Vec2d {
-    system
-    .bodies
-    .iter()
-    .map(|b| b.mass * b.position)
-    .reduce(|v_sum, v| v_sum + v)
-    .expect("Calculation of center of mass should have worked, because we expect that non-empty systems are simulated")
+    let total_mass: f64 = system.bodies.iter().map(|b| b.mass).sum();
+
+    let running_sum: Vec2d = system
+        .bodies
+        .iter()
+        .map(|b| b.mass * b.position)
+        .reduce(|v_sum, v| v_sum + v)
+        .expect("Calculation of center of mass should have worked, because we expect that non-empty systems are simulated");
+
+    running_sum / total_mass
 }
